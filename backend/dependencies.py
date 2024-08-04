ae3c2d34636c84f1348a604f9787d5d051cc301d
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-04 20:57:41 +0530

    initial commit

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,11 +1,13 @@
 from motor import motor_asyncio
 import os
 import dotenv
+from main import get_or_create_eventloop
 
 dotenv.load_dotenv()
 
 mongo = os.getenv("MONGO")
-client = motor_asyncio.AsyncIOMotorClient(mongo)
+loop = get_or_create_eventloop()
+client = motor_asyncio.AsyncIOMotorClient(mongo, io_loop=loop)
 subjects_db = client.subjects
 subjects_collection = subjects_db.get_collection("subjects collection")
 users_db = client.users
