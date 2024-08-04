8817c84e9fc5a692a2d2e66652bf62fe60256e36
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-04 23:58:55 +0530

    initial commit

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -4,17 +4,8 @@ import dotenv
 
 dotenv.load_dotenv()
 
-class MongoDB:
-    client = None
-    
-    @classmethod
-    async def get_client(cls):
-        if cls.client is None:
-            mongo_uri = os.getenv("MONGO")
-            cls.client = motor_asyncio.AsyncIOMotorClient(mongo_uri)
-        return cls.client
-    
-client = MongoDB.get_client()
+mongo_uri = os.getenv("MONGO")
+client = motor_asyncio.AsyncIOMotorClient(mongo_uri)
 subjects_db = client.subjects
 subjects_collection = subjects_db.get_collection("subjects collection")
 users_db = client.users
