ffab9a26e876d809e97c27aa74689234c4ff1b64
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-04 21:16:43 +0530

    initial commit

--- backend/cruds/subjects.py
+++ backend/cruds/subjects.py
@@ -4,44 +4,44 @@ from schemas.subjects import subject_schema, subjects_list
 from typing import List
 from fastapi import HTTPException
 
-def add_subject(subject: Subject) -> Subject:
-    sub = subjects_collection.insert_one(subject.model_dump())
+async def add_subject(subject: Subject) -> Subject:
+    sub = await subjects_collection.insert_one(subject.model_dump())
     if sub.inserted_id:
         return subject
     raise HTTPException(status_code=500, detail="Subject could not be added")
 
-def get_all_subjects() -> List[dict]:
-    subs = subjects_collection.find({}).to_list(None)
+async def get_all_subjects() -> List[dict]:
+    subs = await subjects_collection.find({}).to_list(None)
     return subjects_list(subs)
 
-def get_subjects_by_semester_and_branch(semester_name: str, branch_name: str) -> List[dict]:
-    subs = subjects_collection.find({"semester_name": semester_name, "branch_name": branch_name}).to_list(None)
+async def get_subjects_by_semester_and_branch(semester_name: str, branch_name: str) -> List[dict]:
+    subs = await subjects_collection.find({"semester_name": semester_name, "branch_name": branch_name}).to_list(None)
     if subs:
         return subjects_list(subs)
     return []
 
-def get_subject_by_code(semester_name: str, branch_name: str, subject_code: str) -> dict:
-    sub = subjects_collection.find_one({"semester_name": semester_name, "branch_name": branch_name, "subject_code": subject_code})
+async def get_subject_by_code(semester_name: str, branch_name: str, subject_code: str) -> dict:
+    sub = await subjects_collection.find_one({"semester_name": semester_name, "branch_name": branch_name, "subject_code": subject_code})
     if sub:
         return subject_schema(sub)
     return None
 
-def get_subject_by_type(semester_name: str, branch_name: str, subject_type: str) -> List[dict]:
-    subs = subjects_collection.find({"semester_name": semester_name, "branch_name": branch_name, "subject_type": subject_type}).to_list(None)
+async def get_subject_by_type(semester_name: str, branch_name: str, subject_type: str) -> List[dict]:
+    subs = await subjects_collection.find({"semester_name": semester_name, "branch_name": branch_name, "subject_type": subject_type}).to_list(None)
     if subs:
         return subjects_list(subs)
     return []
 
-def update_subject(semester_name: str, branch_name: str, subject_code: str, subject_data: Subject) -> Subject:
-    sub = subjects_collection.find_one({"semester_name": semester_name, "branch_name": branch_name, "subject_code": subject_code})
+async def update_subject(semester_name: str, branch_name: str, subject_code: str, subject_data: Subject) -> Subject:
+    sub = await subjects_collection.find_one({"semester_name": semester_name, "branch_name": branch_name, "subject_code": subject_code})
     if sub:
-        subjects_collection.update_one({"_id": sub["_id"]}, {"$set": subject_data.model_dump()})
+        await subjects_collection.update_one({"_id": sub["_id"]}, {"$set": subject_data.model_dump()})
         return subject_data
     raise HTTPException(status_code=404, detail="Subject not found")
 
-def delete_subject(semester_name: str, branch_name: str, subject_code: str) -> bool:
-    subject = subjects_collection.find_one({"semester_name": semester_name, "branch_name": branch_name, "subject_code": subject_code})
+async def delete_subject(semester_name: str, branch_name: str, subject_code: str) -> bool:
+    subject = await subjects_collection.find_one({"semester_name": semester_name, "branch_name": branch_name, "subject_code": subject_code})
     if subject:
-        subjects_collection.delete_one({"_id": subject["_id"]})
+        await subjects_collection.delete_one({"_id": subject["_id"]})
         return True
     return False
--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,12 +1,11 @@
 from motor import motor_asyncio
-from pymongo import MongoClient
 import os
 import dotenv
 
 dotenv.load_dotenv()
 
 mongo = os.getenv("MONGO")
-client = MongoClient(mongo)
+client = motor_asyncio.AsyncIOMotorClient(mongo)
 subjects_db = client.subjects
 subjects_collection = subjects_db.get_collection("subjects collection")
 users_db = client.users
