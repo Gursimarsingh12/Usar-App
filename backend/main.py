598fc3d04dbdf30ec849753fe5f3ac119bc6b154
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-04 20:49:29 +0530

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers import subjects, users, notices
 import uvicorn
 import os
-from dependencies import connect_to_mongo, close_mongo_connection
 
 app = FastAPI()
 
@@ -14,9 +13,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.add_event_handler("startup", connect_to_mongo)
-app.add_event_handler("shutdown", close_mongo_connection)
 
 app.include_router(notices.router)
 app.include_router(users.router)
