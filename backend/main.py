3c8a7ef21f258f0f6efcd3fcd84afa7f10e15dca
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-04 19:25:33 +0530

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import subjects, users, notices
 import uvicorn
 import os
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(notices.router)
 app.include_router(users.router)
