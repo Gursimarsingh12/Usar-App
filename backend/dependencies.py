77dd06ccce42c59159bd6e1f2cf7adf084488231
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-04 20:11:41 +0530

    initial commit

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -12,21 +12,24 @@ class MongoDB:
 
     @classmethod
     async def connect(cls):
-        mongo = os.getenv("MONGO")
-        cls.client = AsyncIOMotorClient(mongo)
-        cls.subjects_db = cls.client.subjects
-        cls.subjects_collection = cls.subjects_db.get_collection("subjects collection")
-        cls.users_db = cls.client.users
-        cls.users_collection = cls.users_db.get_collection("users_collection")
-        cls.notices_db = cls.client.notices_db
-        cls.notices_collection = cls.notices_db.get_collection("notices_collection")
+        if cls.client is None:
+            mongo = os.getenv("MONGO")
+            cls.client = AsyncIOMotorClient(mongo)
+            cls.subjects_db = cls.client.subjects
+            cls.subjects_collection = cls.subjects_db.get_collection("subjects collection")
+            cls.users_db = cls.client.users
+            cls.users_collection = cls.users_db.get_collection("users_collection")
+            cls.notices_db = cls.client.notices_db
+            cls.notices_collection = cls.notices_db.get_collection("notices_collection")
 
     @classmethod
     async def close(cls):
-        cls.client.close()
-    
+        if cls.client:
+            cls.client.close()
+
     @classmethod
-    def get_subjects_collection(cls):
+    async def get_subjects_collection(cls):
+        await cls.connect()
         if cls.subjects_collection is None:
-            raise RuntimeError("MongoDB connection not initialized")
+            raise RuntimeError("MongoDB connection nhi chl raha")
         return cls.subjects_collection
