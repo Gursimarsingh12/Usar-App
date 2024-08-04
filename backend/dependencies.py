e8c99ace27594e4fbc2e1a19687a59d719125171
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-04 19:45:17 +0530

    initial commit

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,14 +1,27 @@
-from motor import motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
 import os
 import dotenv
 
 dotenv.load_dotenv()
 
-mongo = os.getenv("MONGO")
-client = motor_asyncio.AsyncIOMotorClient(mongo)
-subjects_db = client.subjects
-subjects_collection = subjects_db.get_collection("subjects collection")
-users_db = client.users
-user_collection = users_db.get_collection("users_collection")
-notices_db = client.notices_db
-notices_collection = notices_db.get_collection("notices_collection")
+class MongoDB:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(MongoDB, cls).__new__(cls)
+            mongo_url = os.getenv("MONGO")
+            cls._instance.client = AsyncIOMotorClient(mongo_url)
+            cls._instance.subjects_db = cls._instance.client.subjects
+            cls._instance.subjects_collection = cls._instance.subjects_db.get_collection("subjects_collection")
+            cls._instance.users_db = cls._instance.client.users
+            cls._instance.user_collection = cls._instance.users_db.get_collection("users_collection")
+            cls._instance.notices_db = cls._instance.client.notices_db
+            cls._instance.notices_collection = cls._instance.notices_db.get_collection("notices_collection")
+        return cls._instance
+
+# Usage
+db_instance = MongoDB()
+subjects_collection = db_instance.subjects_collection
+user_collection = db_instance.user_collection
+notices_collection = db_instance.notices_collection
