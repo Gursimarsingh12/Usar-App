9f19ff18451c131ae4b59039e5936ea053c39322
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-04 21:32:51 +0530

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import subjects, users, notices
 import uvicorn
-import os
+import os, asyncio
 
 app = FastAPI()
 
@@ -18,5 +18,17 @@ app.include_router(notices.router)
 app.include_router(users.router)
 app.include_router(subjects.router)
 
+def get_or_create_event_loop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return loop
+        else:
+            raise
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT")))
+    loop = get_or_create_event_loop()
+    loop.run_until_complete(uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT"))))
