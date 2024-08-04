29970366f88e58f199daaee72a5ba8845f643f80
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-04 20:03:44 +0530

    initial commit

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -6,6 +6,9 @@ dotenv.load_dotenv()
 
 class MongoDB:
     client: AsyncIOMotorClient = None
+    subjects_collection = None
+    users_collection = None
+    notices_collection = None
 
     @classmethod
     async def connect(cls):
@@ -14,7 +17,7 @@ class MongoDB:
         cls.subjects_db = cls.client.subjects
         cls.subjects_collection = cls.subjects_db.get_collection("subjects collection")
         cls.users_db = cls.client.users
-        cls.user_collection = cls.users_db.get_collection("users_collection")
+        cls.users_collection = cls.users_db.get_collection("users_collection")
         cls.notices_db = cls.client.notices_db
         cls.notices_collection = cls.notices_db.get_collection("notices_collection")
 
