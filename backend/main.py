703b773257be0bfa598a4046d8dc8f9120a36c47
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-06 16:15:34 +0530

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from routers import subjects, users, notices
 from dependencies import connect_to_database, close_database_connection
-from fastapi.middleware.wsgi import WSGIMiddleware
+import uvicorn
 from http.server import BaseHTTPRequestHandler
+import os
 
 app = FastAPI()
 
@@ -18,34 +19,31 @@ app.include_router(notices.router)
 app.include_router(users.router)
 app.include_router(subjects.router)
 
-fastapi_app = WSGIMiddleware(app)
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         from io import BytesIO
-        from wsgiref.simple_server import make_server
+        from urllib.parse import urlparse, parse_qs
+        import asyncio
 
-        environ = {
-            'REQUEST_METHOD': self.command,
-            'PATH_INFO': self.path,
-            'SERVER_NAME': self.server.server_address[0],
-            'SERVER_PORT': str(self.server.server_address[1]),
-            'wsgi.input': BytesIO(),
-            'wsgi.errors': BytesIO(),
-            'wsgi.version': (1, 0),
-            'wsgi.url_scheme': 'http',
-            'wsgi.multithread': False,
-            'wsgi.multiprocess': False,
-            'wsgi.run_once': False,
-        }
+        async def app(scope, receive, send):
+            if scope['type'] == 'http':
+                headers = [(k.encode(), v.encode()) for k, v in self.headers.items()]
+                body = await receive()
 
-        def start_response(status, headers):
-            self.send_response(int(status.split(' ')[0]))
-            for header in headers:
-                self.send_header(*header)
-            self.end_headers()
+                send({
+                    'type': 'http.response.start',
+                    'status': 200,
+                    'headers': headers
+                })
+                send({
+                    'type': 'http.response.body',
+                    'body': body
+                })
 
-        result = fastapi_app(environ, start_response)
-        for data in result:
-            self.wfile.write(data)
+        uvicorn.run(app, host='0.0.0.0', port=int(os.getenv("PORT", 8000)))
+
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
         return
