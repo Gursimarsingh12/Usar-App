fe1dcd9297ce5164026d6379809a67a6a86bf9a9
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-06 12:30:46 +0530

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 from routers import subjects, users, notices
 import uvicorn
 import os
+from mangum import Mangum
 
 app = FastAPI()
 
 app.include_router(notices.router)
 app.include_router(users.router)
 app.include_router(subjects.router)
+
+handler = Mangum(app=app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT")))
