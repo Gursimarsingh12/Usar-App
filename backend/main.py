d2965d05ed9d1c0f1fee1cc3eb8546ce18dac072
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-06 16:58:29 +0530

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from routers import subjects, users, notices
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from dependencies import client, connect_to_database, close_database_connection
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
