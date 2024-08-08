c584d96b2c2f21f503a7b4a6d2c498f942d36562
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-08 16:30:31 +0530

    initial commit

--- backend/cruds/notices.py
+++ backend/cruds/notices.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import html
 from schemas.notices import notice_schema, notices_list
-from dependencies import get_notices_collection
 from fastapi import HTTPException
 
 url = "https://sites.google.com/view/ggsipuedc/notice-board?authuser=0"
@@ -35,38 +34,20 @@ def extract_notices(soup):
 
 async def get_notices():
     try:
-        notices_collection = await get_notices_collection()
         soup = fetch_webpage(url)
         notices = extract_notices(soup)
-        await notices_collection.insert_many(notices)
         return notices_list(notices)
     except Exception as e:
-        print(f"Error fetching or storing notices: {e}")
-        raise HTTPException(status_code=500, detail="Error fetching or storing notices")
+        print(f"Error fetching notices: {e}")
+        raise HTTPException(status_code=500, detail="Error fetching notices")
 
 async def get_latest_notices():
     try:
-        notices_collection = await get_notices_collection()
-        await notices_collection.create_index([("title", 1), ("link", 1)], unique=True)
-
         soup = fetch_webpage(url)
         new_notices = extract_notices(soup)
-        latest_notices = []
 
-        existing_titles_links = await notices_collection.find(
-            {}, {"title": 1, "link": 1}
-        ).to_list(length=None)
-        existing_set = {(doc["title"], doc["link"]) for doc in existing_titles_links}
-
-        for notice in new_notices:
-            if (notice["title"], notice["link"]) not in existing_set:
-                latest_notices.append(notice)
-
-        if latest_notices:
-            await notices_collection.insert_many(latest_notices)
-
-        return notices_list(latest_notices)
+        # Simply return the extracted notices as the latest notices
+        return notices_list(new_notices)
     except Exception as e:
-        print(f"Error fetching or storing latest notices: {e}")
-        raise HTTPException(status_code=500, detail="Error fetching or storing latest notices")
-
+        print(f"Error fetching latest notices: {e}")
+        raise HTTPException(status_code=500, detail="Error fetching latest notices")
